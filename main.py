901c3c65145c0b4d59d80fe6bbfcfbdd74dec018
Kind Roach <kindroach@hotmail.com>
2023-05-07 22:45:38 +0800

    Add mode type as argument.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import argparse
+
 import cv2
 import numpy as np
-from openvino.runtime import Core
+from openvino.runtime import Core, CompiledModel
 from tqdm import tqdm
 
 from prepare_data_and_model import IMG_SIZE
 
 
-def total_frames(video_path: str):
+def total_frames(video_path: str) -> int:
     cap = cv2.VideoCapture(video_path)
     assert cap.isOpened()
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -24,14 +26,14 @@ def read_frames(video_path: str):
     cap.release()
 
 
-def sync_infer(model, video_path):
+def sync_infer(model: CompiledModel, video_path: str) -> None:
     for frame in tqdm(read_frames(video_path), total=total_frames(video_path)):
         inputs = cv2.resize(src=frame, dsize=(IMG_SIZE, IMG_SIZE))
         inputs = np.expand_dims(inputs.transpose(2, 0, 1), 0)
         model(inputs)
 
 
-def main():
+def main(model_type: str) -> None:
     ie = Core()
 
     devices = ie.available_devices
@@ -40,10 +42,14 @@ def main():
         device_name = ie.get_property(device, "FULL_DEVICE_NAME")
         print(f"{device}: {device_name}")
 
-    model_xml = "outputs/openvino/fp32/model.xml"
+    model_xml = f"outputs/openvino/{model_type}/model.xml"
     compiled_model = ie.compile_model(model_xml, device_name="CPU")
     sync_infer(compiled_model, "outputs/video.mp4")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_type", type=str, choices=["fp32", "fp16", "int8"], default="fp32")
+    args = parser.parse_args()
+
+    main(args.model_type)
--- prepare_data_and_model.py
+++ prepare_data_and_model.py
@@ -2,83 +2,97 @@ import subprocess
 import urllib.request
 
 import torch
+from openvino.tools.pot import DataLoader
+from openvino.tools.pot import IEEngine
+from openvino.tools.pot import compress_model_weights
+from openvino.tools.pot import create_pipeline
+from openvino.tools.pot import load_model, save_model
+from torch.nn import Module
 from torchvision.models import efficientnet_v2_l, EfficientNet_V2_L_Weights
 
 IMG_SIZE = 480
 
-# download test video
-video_url = "https://storage.openvinotoolkit.org/data/test_data/videos/car-detection.mp4"
-urllib.request.urlretrieve(video_url, "outputs/video.mp4")
 
-# download model
-model = efficientnet_v2_l(weights=EfficientNet_V2_L_Weights.DEFAULT)
-model.eval()
-
-# convert to onnx
-onnx_path = "outputs/model.onnx"
-dummy_input = torch.randn(1, 3, IMG_SIZE, IMG_SIZE)
-torch.onnx.export(model, (dummy_input,), onnx_path)
-
-# convert to openvino
-openvino_path = "outputs/openvino"
-subprocess.run([
-    "mo",
-    "--input_model", onnx_path,
-    "--output_dir", f"{openvino_path}/fp32"
-])
-
-subprocess.run([
-    "mo",
-    "--compress_to_fp16",
-    "--input_model", onnx_path,
-    "--output_dir", f"{openvino_path}/fp16"
-])
-
-# pot quantization
-from openvino.tools.pot import DataLoader
-from openvino.tools.pot import IEEngine
-from openvino.tools.pot import load_model, save_model
-from openvino.tools.pot import compress_model_weights
-from openvino.tools.pot import create_pipeline
-
-dmz_inputs = torch.rand([300, 3, IMG_SIZE, IMG_SIZE], dtype=torch.float32)
+def download_video() -> None:
+    video_url = "https://storage.openvinotoolkit.org/data/test_data/videos/car-detection.mp4"
+    urllib.request.urlretrieve(video_url, "outputs/video.mp4")
 
 
-class DmzLoader(DataLoader):
-    def __init__(self):
-        super().__init__(None)
-
-    def __len__(self):
-        return len(dmz_inputs)
-
-    def __getitem__(self, index):
-        # annotation is set to None
-        return dmz_inputs[index], None
+def download_model() -> Module:
+    model = efficientnet_v2_l(weights=EfficientNet_V2_L_Weights.DEFAULT)
+    model.eval()
+    return model
 
 
-model = load_model(model_config={
-    "model_name": "model",
-    "model": f"{openvino_path}/fp32/model.xml",
-    "weights": f"{openvino_path}/fp32/model.bin",
-})
+def convert_torch_to_openvino(model: Module) -> str:
+    # convert to onnx
+    onnx_path = "outputs/model.onnx"
+    dummy_input = torch.randn(1, 3, IMG_SIZE, IMG_SIZE)
+    torch.onnx.export(model, (dummy_input,), onnx_path)
 
-engine = IEEngine(config={"device": "CPU"}, data_loader=DmzLoader())
+    # convert to openvino
+    openvino_path = "outputs/openvino"
+    subprocess.run([
+        "mo",
+        "--input_model", onnx_path,
+        "--output_dir", f"{openvino_path}/fp32"
+    ])
+    subprocess.run([
+        "mo",
+        "--compress_to_fp16",
+        "--input_model", onnx_path,
+        "--output_dir", f"{openvino_path}/fp16"
+    ])
 
-algorithms = [
-    {
-        "name": "DefaultQuantization",
-        "params": {
-            "target_device": "ANY",
-            "stat_subset_size": 300,
-            "stat_batch_size": 1
-        },
-    }
-]
-pipeline = create_pipeline(algorithms, engine)
-compressed_model = pipeline.run(model=model)
-compress_model_weights(compressed_model)
-compressed_model_paths = save_model(
-    model=compressed_model,
-    save_path=f"{openvino_path}/int8",
-    model_name="model",
-)
+    return openvino_path
+
+
+def quantization(openvino_path: str) -> None:
+    dmz_inputs = torch.rand([300, 3, IMG_SIZE, IMG_SIZE], dtype=torch.float32)
+
+    class DmzLoader(DataLoader):
+        def __init__(self):
+            super().__init__(None)
+
+        def __len__(self):
+            return len(dmz_inputs)
+
+        def __getitem__(self, index):
+            # annotation is set to None
+            return dmz_inputs[index], None
+
+    model = load_model(model_config={
+        "model_name": "model",
+        "model": f"{openvino_path}/fp32/model.xml",
+        "weights": f"{openvino_path}/fp32/model.bin",
+    })
+    engine = IEEngine(config={"device": "CPU"}, data_loader=DmzLoader())
+    algorithms = [
+        {
+            "name": "DefaultQuantization",
+            "params": {
+                "target_device": "ANY",
+                "stat_subset_size": 300,
+                "stat_batch_size": 1
+            },
+        }
+    ]
+    pipeline = create_pipeline(algorithms, engine)
+    compressed_model = pipeline.run(model=model)
+    compress_model_weights(compressed_model)
+    save_model(
+        model=compressed_model,
+        save_path=f"{openvino_path}/int8",
+        model_name="model",
+    )
+
+
+def main():
+    download_video()
+    model = download_model()
+    path = convert_torch_to_openvino(model)
+    quantization(path)
+
+
+if __name__ == '__main__':
+    main()
