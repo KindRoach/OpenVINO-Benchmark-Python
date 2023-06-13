956a3c271a44282a250730467707dfbc887dab86
Kind Roach <kindroach@hotmail.com>
2023-06-13 23:45:36 +0800

    Use cmd for args input in test.

--- test/test_opencv.py
+++ test/test_opencv.py
@@ -5,14 +5,13 @@ from opencv_benchmark import sync_decode, multi_stream_decode
 
 class TestingSyncDecode(unittest.TestCase):
     def test_sync_decode(self):
-        test_args = sync_decode.parse_args([])
-        test_args.run_time = 5
+        cmd = "-t 5"
+        test_args = sync_decode.parse_args(cmd.split())
         sync_decode.main(test_args)
 
 
 class TestingMultiStreamDecode(unittest.TestCase):
     def test_main(self):
-        test_args = multi_stream_decode.parse_args([])
-        test_args.run_time = 5
-        test_args.n_stream = 2
+        cmd = "-t 5 -n 2"
+        test_args = multi_stream_decode.parse_args(cmd.split())
         multi_stream_decode.main(test_args)
--- test/test_openvino.py
+++ test/test_openvino.py
@@ -5,22 +5,30 @@ from openvino_benchmark import sync_infer, async_infer, multi_stream_infer
 
 class TestingSyncInfer(unittest.TestCase):
     def test_sync_decode(self):
-        test_args = sync_infer.parse_args([])
-        test_args.run_time = 5
+        cmd = "-t 5"
+        test_args = sync_infer.parse_args(cmd.split())
+        sync_infer.main(test_args)
+
+    def test_main_fp32(self):
+        cmd = "-t 5 -p fp32"
+        test_args = sync_infer.parse_args(cmd.split())
+        sync_infer.main(test_args)
+
+    def test_main_fp16(self):
+        cmd = "-t 5 -p fp16"
+        test_args = sync_infer.parse_args(cmd.split())
         sync_infer.main(test_args)
 
 
 class TestingAsyncInfer(unittest.TestCase):
     def test_main(self):
-        test_args = async_infer.parse_args([])
-        test_args.run_time = 5
-        test_args.infer_jobs = 2
+        cmd = "-t 5 -n 2"
+        test_args = async_infer.parse_args(cmd.split())
         async_infer.main(test_args)
 
 
 class TestingMultiStreamInfer(unittest.TestCase):
     def test_main(self):
-        test_args = multi_stream_infer.parse_args([])
-        test_args.run_time = 5
-        test_args.n_stream = 2
+        cmd = "-t 5 -n 2"
+        test_args = multi_stream_infer.parse_args(cmd.split())
         multi_stream_infer.main(test_args)
